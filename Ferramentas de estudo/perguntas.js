06e4275a1f2562f79b78bb284bb9be68146763eb
Geisiane3C <00001093657480sp@al.educacao.sp.gov.br>
2024-11-21 10:33:38 -0300

    Add files via upload

---
+++ Ferramentas de estudo/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é Index?',
+    'O Index é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital de Minas Gerais?',
+    'A capital de Minas Gerais é Belo Horizonte'
+)
+
+criaCartao(
+    'Programação',
+    'O que é um Javascript?',
+    'Permite a você criar conteúdo que se atualiza dinamicamente, controlar múltimídias e imagens'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz estou bem em Inglês?',
+    'se diz Im fine (EYEM FYN)'
+)
